c024fe9f8cb1343f8a9d3219f1569941257e0b18
jayak <kumarijaya127@gmail.com>
2024-05-17 19:40:00 +0530

    product-model setup

---
+++ models/productModel.js
@@ -0,0 +1,17 @@
+const { Db } = require("mongodb");
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  image: String,
+  name: String,
+  price: Number,
+  discount: {
+    type: number,
+    default: 0,
+  },
+  bgColor: String,
+  panelColor: String,
+  textColor: String,
+});
+
+module.exports = mongoose.model("product", productSchema);
